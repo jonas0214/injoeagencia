283537c8c13dc1a0e99c82c9db9316770b4d0ea1
Jonathan Cabrera <jonas021@gmail.com>
2026-02-15 09:34:17 -0500

    correcciondes Alpine

--- limbani-app/resources/views/layouts/asana.blade.php
+++ limbani-app/resources/views/layouts/asana.blade.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Alpine.js Plugins -->
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+    <!-- Alpine.js Core -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <style>
